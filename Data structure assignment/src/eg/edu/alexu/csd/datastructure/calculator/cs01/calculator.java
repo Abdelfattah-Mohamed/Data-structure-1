a5ea868745a6bf47931a5415a9480317e31d21a2
Abd El Fattah <Abd El Fattah@LAPTOP-Q29UUR9I>
2018-02-14 16:19:48 +0200

    This is my first commit to git

---
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/calculator/cs01/calculator.java
@@ -0,0 +1,20 @@
+package eg.edu.alexu.csd.datastructure.calculator.cs01;
+
+import eg.edu.alexu.csd.datastructure.calculator.ICalculator;
+
+public class calculator implements ICalculator {
+
+	@Override
+	public int add(int x, int y) {
+		// TODO Auto-generated method stub
+		return x + y;
+	}
+
+	@Override
+	public float divide(int x, int y) throws RuntimeException {
+		// TODO Auto-generated method stub
+		throw 
+		return (flout) x / y ;
+	}
+
+}
