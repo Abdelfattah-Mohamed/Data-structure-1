3106f301c6d8f1b837b36c94a5f1d1165270d94b
Abd El Fattah <Abd El Fattah@LAPTOP-Q29UUR9I>
2018-02-14 17:15:27 +0200

    style

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/calculator/cs26/Calculator.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/calculator/cs26/Calculator.java
@@ -5,12 +5,12 @@ import eg.edu.alexu.csd.datastructure.calculator.ICalculator;
 public class Calculator implements ICalculator {
 
 	@Override
-   	public int add(final int x,final  int y) {
+   	public int add(final int x, final int y) {
 		return x + y;
 	}
 
 	@Override
-	public float divide(final int x,final int y) throws RuntimeException {
+	public float divide(final int x, final int y) throws RuntimeException {
 		if (y == 0) {
 			throw new RuntimeException("Division by zero!");
 		}
