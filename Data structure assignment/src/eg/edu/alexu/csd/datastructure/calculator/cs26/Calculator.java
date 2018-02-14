a9c74581f029b77e947208fc133858158a3cbd40
Abd El Fattah <Abd El Fattah@LAPTOP-Q29UUR9I>
2018-02-14 17:14:50 +0200

    style try

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/calculator/cs26/Calculator.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/calculator/cs26/Calculator.java
@@ -5,7 +5,7 @@ import eg.edu.alexu.csd.datastructure.calculator.ICalculator;
 public class Calculator implements ICalculator {
 
 	@Override
-   	public int add(final int x,final int y) {
+   	public int add(final int x,final  int y) {
 		return x + y;
 	}
 
