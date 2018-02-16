efa4a814402ecd59b0cb44f24a4e89a7ebc37d7a
Abd El Fattah <Abd El Fattah@LAPTOP-Q29UUR9I>
2018-02-16 23:29:58 +0200

    1

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/calculator/cs26/Calculator.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/calculator/cs26/Calculator.java
@@ -17,12 +17,12 @@ public class Calculator implements ICalculator {
 	}
 
 	@Override
-	public float divide(final int x, final int y) throws RuntimeException {
+	public float divide(int x, int y) throws RuntimeException {
 		// TODO Auto-generated method stub
 		if (y == 0) {
 			throw new RuntimeException("Division by zero!");
 		}
 
-		return (float) x / y ;
+		return (float) x / y;
 	}
 }
