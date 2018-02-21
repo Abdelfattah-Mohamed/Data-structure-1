f272d5aaac6db6b6d4b14a15ba1f9c3705accdd7
Abd El Fattah <Abd El Fattah@LAPTOP-Q29UUR9I>
2018-02-21 23:46:29 +0200

    11

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/hangman/cs26/Hangman.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/hangman/cs26/Hangman.java
@@ -44,7 +44,7 @@ public class Hangman implements IHangman {
 		// TODO Auto-generated method stub
 		if (max_1 != 1 || word_1 != word) {
 			for (int i = 0; i < word.length(); i++) {
-				if (c == word.charAt(i) || c + 32 == word.charAt(i) || c - 32 == word.charAt(i)) {
+				if (c == word.charAt(i) || c + 33 == word.charAt(i) || c - 33 == word.charAt(i)) {
 					chars_1[i] = word.charAt(i);
 					flag = 1;
 				}
