585cfb96085c12bb8ece47f2f31e99b1de01b087
Abd El Fattah <Abd El Fattah@LAPTOP-Q29UUR9I>
2018-02-22 16:49:45 +0200

    style 28

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/hangman/cs26/HangmanTester.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/hangman/cs26/HangmanTester.java
@@ -6,10 +6,11 @@ package eg.edu.alexu.csd.datastructure.hangman.cs26;
 public class HangmanTester {
 
 	public static void main(final String[] args) throws Exception {
+		// TODO Auto-generated constructor stub
 		Hangman hangman = new Hangman();
 		// Read from file here
 		// String dictionary[] = readFile();
-		String[] dictionary = new String[]{};
+		String[] dictionary = new String[]{ "Egypt" };
 		hangman.setDictionary(dictionary);
 		hangman.selectRandomSecretWord();
 		hangman.setMaxWrongGuesses(2);
