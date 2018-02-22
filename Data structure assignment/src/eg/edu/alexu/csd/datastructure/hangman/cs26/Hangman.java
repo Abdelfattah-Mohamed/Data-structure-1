4131f9019b78beebb9426f5449eb55c4a2166451
Abd El Fattah <Abd El Fattah@LAPTOP-Q29UUR9I>
2018-02-22 16:42:59 +0200

    STYLE 23

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/hangman/cs26/Hangman.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/hangman/cs26/Hangman.java
@@ -51,7 +51,7 @@ public class Hangman implements IHangman {
 	public String guess(final Character c) throws Exception {
 		// TODO Auto-generated method stub
 		if (word.charAt(0) == ' ' || word.length() == 0) {
-			throw new UnsupportedOperationException();
+			throw new RuntimeException();
 		}
 		if (c == null) {
 			return "------";
