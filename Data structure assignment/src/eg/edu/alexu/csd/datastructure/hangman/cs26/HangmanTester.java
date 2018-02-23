9787b3ded1265581eb93427b9db341992b550afc
Abd El Fattah <Abd El Fattah@LAPTOP-Q29UUR9I>
2018-02-23 16:30:49 +0200

    s

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/hangman/cs26/HangmanTester.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/hangman/cs26/HangmanTester.java
@@ -1,19 +1,22 @@
 package eg.edu.alexu.csd.datastructure.hangman.cs26;
+
 /**
  * @author Abdelftah
  *
  */
 public class HangmanTester {
 
-	/** 
-	 * @param args for initial
-	 * @throws Exception to print return of hangman.guess.
+	/**
+	 * @param args
+	 *            for initial
+	 * @throws Exception
+	 *             to print return of hangman.guess.
 	 */
 	public static void main(final String[] args) throws Exception {
 		Hangman hangman = new Hangman();
 		// Read from file here
 		// String dictionary[] = readFile();
-		String[] dictionary = new String[]{};
+		String[] dictionary = new String[] {};
 		hangman.setDictionary(dictionary);
 		hangman.selectRandomSecretWord();
 		hangman.setMaxWrongGuesses(2);
