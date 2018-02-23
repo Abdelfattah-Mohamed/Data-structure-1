4a80d5099d7a879759f5007bd26cc61ab68c82e9
Abd El Fattah <Abd El Fattah@LAPTOP-Q29UUR9I>
2018-02-23 14:55:04 +0200

    style 47

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/hangman/cs26/Hangman.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/hangman/cs26/Hangman.java
@@ -10,45 +10,39 @@ import eg.edu.alexu.csd.datastructure.hangman.IHangman;
  */
 
 public class Hangman implements IHangman {
-	/** 
-	 * to store dictionary words 
+	/**
+	 *to store dictionary words
 	 */
 	private static String word, word1;
 	/**
-	 * to store dictionary words
+	 *to store dictionary words
 	 */
 	private static String[] newArray;
 	/**
-	 * store letter of the random word
+	 *store letter of the random word
 	 */
 	@SuppressWarnings("unused")
 	private static char[] chars;
 	/**
-	 * to print dashes 
+	 *to print dashes
 	 */
 	private static char[] chars1;
 	/**
-	 * to know wrong or write guess
+	 *to know wrong or write guess
 	 */
 	private static int flag = 0;
 	/**
-	 * to store max number of guess
+	 *to store max number of guess
 	 */
 	private static Integer max1 = null;
 
 	@Override
-	/**
-	 * 
-	 */
 	public void setDictionary(final String[] words) {
 		// TODO Auto-generated method stub
 		newArray = words;
 	}
 
 	@Override
-	/**
-	 * 
-	 */
 	public String selectRandomSecretWord() {
 		// TODO Auto-generated method stub
 		if (newArray == null) {
@@ -70,9 +64,6 @@ public class Hangman implements IHangman {
 	}
 
 	@Override
-	/**
-	 * 
-	 */
 	public String guess(final Character c) throws Exception {
 		// TODO Auto-generated method stub
 		if (word.charAt(0) == ' ' || word.length() == 0) {
@@ -113,9 +104,6 @@ public class Hangman implements IHangman {
 	}
 
 	@Override
-	/**
-	 * 
-	 */
 	public void setMaxWrongGuesses(final Integer max) {
 		// TODO Auto-generated method stub
 		if (max == null) {
