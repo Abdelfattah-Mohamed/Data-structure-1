cbdc119163ab2f82e06425fc883fbfba89153833
Abd El Fattah <Abd El Fattah@LAPTOP-Q29UUR9I>
2018-02-26 00:06:42 +0200

    prepare assignment 3

---
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/iceHockey/cs26/Icehocky.java
@@ -0,0 +1,21 @@
+package eg.edu.alexu.csd.datastructure.iceHockey.cs26;
+
+import java.awt.Point;
+
+import eg.edu.alexu.csd.datastructure.iceHockey.IPlayersFinder;
+
+public class Icehocky implements IPlayersFinder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Point[] findPlayers(String[] photo, int team, int threshold) {
+		// TODO Auto-generated method stub
+		
+		return null;
+	}
+
+}
