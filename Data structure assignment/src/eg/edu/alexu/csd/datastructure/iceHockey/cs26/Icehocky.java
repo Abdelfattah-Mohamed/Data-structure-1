b15f649c3b0f4234ec843ffdec0a69641d9a839f
Abd El Fattah <Abd El Fattah@LAPTOP-Q29UUR9I>
2018-02-27 13:33:52 +0200

    first method

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/iceHockey/cs26/Icehocky.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/iceHockey/cs26/Icehocky.java
@@ -14,8 +14,29 @@ public class Icehocky implements IPlayersFinder {
 	@Override
 	public Point[] findPlayers(String[] photo, int team, int threshold) {
 		// TODO Auto-generated method stub
-		
+		char photoArray[][] = null;
+		storePhoteIn2DimintionArray(photo, photoArray, team);
+
 		return null;
+	}
+
+	private void storePhoteIn2DimintionArray(String[] photo, char photoArray[][], int team) {
+		int i, j;
+		for (i = 0; i < (photo.length); i++) {
+			for (j = 0; photo[i].charAt(j) != '\0'; j++) {
+				if (photo[i].charAt(j) == team) {
+					photoArray[i][j] = photo[i].charAt(j);
+				} else {
+					photoArray[i][j] = '0';
+				}
+			}
+		}
+	}
+
+	private int recursion(int i, int j, int arr[][]) {
+		final int counter = 0;
+
+		return counter;
 	}
 
 }
