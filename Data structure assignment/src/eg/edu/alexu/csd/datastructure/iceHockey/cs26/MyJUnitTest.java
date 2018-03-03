31e7aac930598777868f1111ba18cebeff68db49
Abd El Fattah <Abd El Fattah@LAPTOP-Q29UUR9I>
2018-03-03 17:54:06 +0200

    1

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/iceHockey/cs26/MyJUnitTest.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/iceHockey/cs26/MyJUnitTest.java
@@ -17,8 +17,170 @@ public class MyJUnitTest {
 	@Test
 	public void iceHockeyTest() {
 		IPlayersFinder playersFinder = new Icehocky();
-		String[] image = {};
-		Point[] answer = new Point[] {};
+		String[] image = {
+				"33JUBU33", 
+                "3U3O4433", 
+                "O33P44NB", 
+                "PO3NSDP3", 
+                "VNDSD333", 
+                "OINFD33X"
+		};
+		Point[] answer = 
+			new Point[] {
+				new Point(4, 5),
+                new Point(13, 9),
+                new Point(14, 2),
+		};
+		final int team = 3;
+		final int threashold = 16;
+		Assert.assertArrayEquals(answer,
+				playersFinder.findPlayers(image,
+						team, threashold));
+	}
+	@Test
+	public void iceHockeyTest1() {
+		IPlayersFinder playersFinder = new Icehocky();
+		String[] image = {
+				"33333333333333333333333333333333333333333333333333", 
+		};
+		Point[] answer = 
+			new Point[] {
+				new Point(50, 1),
+		};
+		final int team = 3;
+		final int threashold = 5;
+		Assert.assertArrayEquals(answer,
+				playersFinder.findPlayers(image,
+						team, threashold));
+	}
+	@Test
+	public void iceHockeyTest2() {
+		IPlayersFinder playersFinder = new Icehocky();
+		String[] image = {
+				"3", 
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+				"3", 
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+				"3", 
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+				"3", 
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+				"3", 
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+				"3",
+		};
+		Point[] answer = 
+			new Point[] {
+				new Point(1, 50),
+		};
+		final int team = 3;
+		final int threashold = 3;
+		Assert.assertArrayEquals(answer,
+				playersFinder.findPlayers(image,
+						team, threashold));
+	}
+	@Test
+	public void iceHockeyTest3() {
+		IPlayersFinder playersFinder = new Icehocky();
+		String[] image = {
+				"33333333", 
+                "33333333", 
+                "33333333", 
+                "33333333", 
+                "33333333",
+                "33333333",
+                "33333333",
+                "33333333"
+		};
+		Point[] answer = 
+			new Point[] {
+				new Point(8, 8),
+		};
+		final int team = 3;
+		final int threashold = 12;
+		Assert.assertArrayEquals(answer,
+				playersFinder.findPlayers(image,
+						team, threashold));
+	}
+	@Test
+	public void iceHockeyTest4() {
+		IPlayersFinder playersFinder = new Icehocky();
+		String[] image = {
+				"313r3d3c", 
+                "b3v3z3x3", 
+                "3v3d3t3y", 
+                "137343n3", 
+                "3z3c3v3b"
+		};
+		Point[] answer = 
+			new Point[] {
+		};
+		final int team = 3;
+		final int threashold = 5;
+		Assert.assertArrayEquals(answer,
+				playersFinder.findPlayers(image,
+						team, threashold));
+	}
+	@Test
+	public void iceHockeyTest5() {
+		IPlayersFinder playersFinder = new Icehocky();
+		String[] image = null ;
+		Point[] answer = null ;
+		final int team = 3;
+		final int threashold = 12;
+		Assert.assertArrayEquals(answer,
+				playersFinder.findPlayers(image,
+						team, threashold));
+	}
+	@Test
+	public void iceHockeyTest6() {
+		IPlayersFinder playersFinder = new Icehocky();
+		String[] image = {
+				"1"
+		};
+		Point[] answer = 
+			new Point[] {
+				new Point(1, 1),
+		};
 		final int team = 1;
 		final int threashold = 3;
 		Assert.assertArrayEquals(answer,
