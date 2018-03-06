53dbb02615ce1117c7dd87caddbc50cd515d7ec8
Abd El Fattah <Abd El Fattah@LAPTOP-Q29UUR9I>
2018-03-06 23:48:04 +0200

    1

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/iceHockey/cs26/MyJUnitTest.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/iceHockey/cs26/MyJUnitTest.java
@@ -209,4 +209,35 @@ public class MyJUnitTest {
 				playersFinder.findPlayers(image,
 						team, threashold));
 	}
+	/**
+	 * @Test
+	 */
+	@Test
+	public void randomPhoto4() {
+		IPlayersFinder playersFinder = new Icehocky();
+		final String[] photo = {
+				"44444H44S4",
+				"K444K4L444",
+				"4LJ44T44XH",
+				"444O4VIF44",
+				"44C4D4U444",
+				"4V4Y4KB4M4",
+				"G4W4HP4O4W",
+				"4444ZDQ4S4",
+				"4BR4Y4A444",
+				"4G4V4T4444"};
+		final int team = 4;
+		final int threshold = 16;
+		Point[] returnedPoints;
+		returnedPoints = playersFinder.findPlayers(photo, team,
+				threshold);
+		final Point[] ans = {
+				new Point(3, 8),
+				new Point(4, 16),
+				new Point(5, 4),
+				new Point(16, 3),
+				new Point(16, 17),
+				new Point(17, 9)};
+		Assert.assertArrayEquals(ans, returnedPoints);
+	}
 }
