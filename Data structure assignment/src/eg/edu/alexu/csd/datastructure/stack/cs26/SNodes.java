ab684115564b725809373becde0d687a21dd5c36
Abd El Fattah <Abd El Fattah@Abdelfattah>
2018-03-28 01:51:08 +0200

    5

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/stack/cs26/SNodes.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/stack/cs26/SNodes.java
@@ -6,7 +6,7 @@ public class SNodes<T> {
 	SNodes<T> next;
 	SNodes<T> prev;
 
-	public SNodes(T value, SNodes<T> next, SNodes<T> prev) {
+	public SNodes(final T value, final SNodes<T> next, final SNodes<T> prev) {
 		// TODO Auto-generated constructor stub
 		this.value = value;
 		this.next = next;
