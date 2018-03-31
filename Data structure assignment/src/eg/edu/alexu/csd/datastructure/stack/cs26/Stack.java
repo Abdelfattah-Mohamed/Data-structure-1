602b15313985d6fbe4d9f5f1a7730a299acc9fda
Abd El Fattah <Abd El Fattah@Abdelfattah>
2018-03-31 19:38:40 +0200

    1

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/stack/cs26/Stack.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/stack/cs26/Stack.java
@@ -2,6 +2,23 @@ package eg.edu.alexu.csd.datastructure.stack.cs26;
 
 import eg.edu.alexu.csd.datastructure.stack.IStack;
 
+
+class SNodes<T> {
+
+	T value;
+	SNodes<T> next;
+	SNodes<T> prev;
+
+	public SNodes(final T value,
+			final SNodes<T> next, final SNodes<T> prev) {
+		// TODO Auto-generated constructor stub
+		this.value = value;
+		this.next = next;
+		this.prev = prev;
+	}
+
+}
+
 public class Stack implements IStack {
 
 	SNodes<Object> head = null;
