78c28f38565861cba9ba03131b0098940bdbe70f
Abd El Fattah <Abd El Fattah@Abdelfattah>
2018-04-02 16:22:36 +0200

    1

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/linkedList/cs25_cs26/SingleLinkedList.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/linkedList/cs25_cs26/SingleLinkedList.java
@@ -22,27 +22,7 @@ public class SingleLinkedList implements ILinkedList {
 	@Override
 	public void add(int index, Object element) {
 		// TODO Auto-generated method stub
-		int x = index;
-		if (x > size) {
-			throw new IndexOutOfBoundsException();
-		}
-		Snode nptr = new Snode(element, null);
-		Snode q = head;
-		if (q == null || x == 0) {
-			nptr.next = head;
-			head = nptr;
-		}
-		for (; q != null;) {
-			if (x == 1) {
-				Snode temp = q.next;
-				q.next = nptr;
-				nptr.next = temp;
-				break;
-			}
-			q = q.next;
-			x--;
-		}
-		size++;
+		
 	}
 
 	@Override
