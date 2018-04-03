3e7a74ceb3598fa48e46446bfbcc45858396bf7f
Abd El Fattah <Abd El Fattah@Abdelfattah>
2018-04-03 20:39:46 +0200

    1

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/linkedList/cs25_cs26/Snode.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/linkedList/cs25_cs26/Snode.java
@@ -5,6 +5,13 @@ public class Snode {
 	Object value;
 	Snode next;
 
+	/**
+	 * 
+	 * @param value
+	 *            element of the node.
+	 * @param next
+	 *            point to the next node.
+	 */
 	public Snode(final Object value, final Snode next) {
 		this.value = value;
 		this.next = next;
