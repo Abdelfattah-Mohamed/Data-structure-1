670c1cfe4aba591ce71683d3d7eef93be3af74bb
Abd El Fattah <Abd El Fattah@Abdelfattah>
2018-04-04 03:34:00 +0200

    t3

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/stack/cs26/InfixPosfix.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/stack/cs26/InfixPosfix.java
@@ -56,7 +56,7 @@ public class InfixPosfix implements IExpressionEvaluator {
 			str = str + store[j] + " ";
 		}
 		*/
-		String str = String.valueOf(store[1]);
+		String str = String.valueOf(store[2]);
 		return str;
 	}
 
