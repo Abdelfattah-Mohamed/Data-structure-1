653f55471ef76ead4ee30dea9005a1110cd9cb2c
Abd El Fattah <Abd El Fattah@Abdelfattah>
2018-04-04 02:15:45 +0200

    pi1

---
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/stack/cs26/InfixPosfix.java
@@ -0,0 +1,24 @@
+package eg.edu.alexu.csd.datastructure.stack.cs26;
+
+import eg.edu.alexu.csd.datastructure.stack.IExpressionEvaluator;
+
+public class InfixPosfix implements IExpressionEvaluator {
+
+	Dnode head = null;
+	Dnode tail = null;
+	int size = 0;
+
+	@Override
+	public String infixToPostfix(String expression) {
+		// TODO Auto-generated method stub
+		
+		return null;
+	}
+
+	@Override
+	public int evaluate(String expression) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
