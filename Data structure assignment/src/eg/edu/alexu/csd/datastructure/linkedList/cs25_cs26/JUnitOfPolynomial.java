8b1e53990a49b34bb4a033f97e08c32b7e43120f
Abd El Fattah <Abd El Fattah@Abdelfattah>
2018-04-05 05:51:42 +0200

    1

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/linkedList/cs25_cs26/JUnitOfPolynomial.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/linkedList/cs25_cs26/JUnitOfPolynomial.java
@@ -1,11 +1,9 @@
 package eg.edu.alexu.csd.datastructure.linkedList.cs25_cs26;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
