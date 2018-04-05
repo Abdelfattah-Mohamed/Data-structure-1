b9cb49153aa93baad1132bbed2271bf4135b33c0
Abd El Fattah <Abd El Fattah@Abdelfattah>
2018-04-05 06:45:31 +0200

    PI

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/stack/cs26/InfixPosfix.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/stack/cs26/InfixPosfix.java
@@ -34,7 +34,7 @@ public class InfixPosfix implements IExpressionEvaluator {
 		for (; i < expression.length(); i++) {
 			if (expression.charAt(i) != '+' && expression.charAt(i) != '-' && expression.charAt(i) != '/'
 					&& expression.charAt(i) != '*') {
-				if (expression.charAt(i) == '~' || expression.charAt(i) == '!') {
+				if (expression.charAt(i) == '~' || expression.charAt(i) == '!' || expression.charAt(i) == '&') {
 					throw new RuntimeException();
 				}
 				store[l] = expression.charAt(i);
