05189f36e4f664f9f74a6b14ea9aad52100f38ce
Abd El Fattah <Abd El Fattah@Abdelfattah>
2018-04-05 06:13:26 +0200

    S

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/linkedList/cs25_cs26/JUnitOfPolynomial.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/linkedList/cs25_cs26/JUnitOfPolynomial.java
@@ -24,14 +24,14 @@ public class JUnitOfPolynomial {
 	public void cs55TestSizeContains() {
 		IPolynomialSolver instance = (IPolynomialSolver) new Polynomial();
 		ILinkedList object = (ILinkedList) new SingleLinkedList();
-		instance.setPolynomial('C', new int[][] {{3, 7}, {45, 5}, {176, 3}, {128, 1} });
-		instance.setPolynomial('B', new int[][] {{-120, 5}, {-1, 3}, {27, 2}, {1, 1}, {-1, 0} });
-		instance.setPolynomial('A', new int[][] {{120, 5}, {1, 3}, {-27, 2}, {-1, 1}, {1, 0} });
+		instance.setPolynomial('C', new int[][] { { 3, 7 }, { 45, 5 }, { 176, 3 }, { 128, 1 } });
+		instance.setPolynomial('B', new int[][] { { -120, 5 }, { -1, 3 }, { 27, 2 }, { 1, 1 }, { -1, 0 } });
+		instance.setPolynomial('A', new int[][] { { 120, 5 }, { 1, 3 }, { -27, 2 }, { -1, 1 }, { 1, 0 } });
 		assertNull("Polynomial R is not set yet", instance.print('R'));
 		int[][] result1 = instance.add('C', 'B');
 		float a = instance.evaluatePolynomial('A', 10);
 		assertNotNull("Polynomial R must be set after evaluation", instance.print('R'));
-		int[][] expected = new int[][] {{3, 7 }, {-75, 5 }, {175, 3 }, {27, 2 }, {129, 1 }, {-1, 0 } };
+		int[][] expected = new int[][] { { 3, 7 }, { -75, 5 }, { 175, 3 }, { 27, 2 }, { 129, 1 }, { -1, 0 } };
 		System.out.println();
 		for (int i = 0; i < result1.length; i++) {
 			System.out.println(result1[i][1]);
@@ -40,51 +40,50 @@ public class JUnitOfPolynomial {
 		System.out.println();
 		System.out.println(x);
 	}
+
 	/**
 	 * TO TEST LINKED.
 	 */
 	@Test
 	public void cs56TestAddRemoveTwoLists2() {
 		ILinkedList c = (ILinkedList) new SingleLinkedList();
-		for (int i=0; i<3; i++) {
-            c.add(i);
-        }
-        c.add(0, 3);
-        c.add(4, 4);
-        
-        assertTrue(c.contains(4));
-        assertTrue(c.contains(0));
-        assertTrue(c.contains(1));
-        assertTrue(c.contains(2));
-        assertTrue(c.contains(3));
-        assertFalse(c.contains(9));
-        assertFalse(c.contains(7));
-        ILinkedList d = (ILinkedList) new SingleLinkedList();
-        d.add(3);
-        d.add(3);
-        d.add(3);
-        d.add(3);
-        d.add(3);
-        d.add(3);
-        d.add(3);
-        d.add(3);
-        d.add(3);
-        d.add(3);
-        d.add(3);
-        d.add(3);
-        d.add(3);
-        d.add(3);
-        d.add(3);
-        d.add(3);
-        d.add(3);
-        d.add(3);
-        d.add(3);
-        d.add(3);
-        assertTrue(d.contains(3));
-        /*
-        for (int i=0;i<5;i++){
-            assertEquals(c.get(i),d.get(i));
-        }
-        */
-    }
+		for (int i = 0; i < 3; i++) {
+			c.add(i);
+		}
+		c.add(0, 3);
+		c.add(4, 4);
+
+		assertTrue(c.contains(4));
+		assertTrue(c.contains(0));
+		assertTrue(c.contains(1));
+		assertTrue(c.contains(2));
+		assertTrue(c.contains(3));
+		assertFalse(c.contains(9));
+		assertFalse(c.contains(7));
+		ILinkedList d = (ILinkedList) new SingleLinkedList();
+		d.add(3);
+		d.add(3);
+		d.add(3);
+		d.add(3);
+		d.add(3);
+		d.add(3);
+		d.add(3);
+		d.add(3);
+		d.add(3);
+		d.add(3);
+		d.add(3);
+		d.add(3);
+		d.add(3);
+		d.add(3);
+		d.add(3);
+		d.add(3);
+		d.add(3);
+		d.add(3);
+		d.add(3);
+		d.add(3);
+		assertTrue(d.contains(3));
+		/*
+		 * for (int i=0;i<5;i++){ assertEquals(c.get(i),d.get(i)); }
+		 */
+	}
 }
