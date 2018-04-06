759479f9b2fd160795d283c71b5a76d5d60bdcb7
Abd El Fattah <Abd El Fattah@Abdelfattah>
2018-04-07 00:58:10 +0200

    pi

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/stack/cs26/InfixPosfix.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/stack/cs26/InfixPosfix.java
@@ -24,7 +24,7 @@ public class InfixPosfix implements IExpressionEvaluator {
 	@Override
 	public String infixToPostfix(final String expression) {
 		// TODO Auto-generated method stub
-		char[] store = new char[2 * expression.length() - 1];
+		char[] store = new char[2 * expression.length()];
 		Stack a = (Stack) new Stack();
 		int i = 0;
 		int l = 0;
