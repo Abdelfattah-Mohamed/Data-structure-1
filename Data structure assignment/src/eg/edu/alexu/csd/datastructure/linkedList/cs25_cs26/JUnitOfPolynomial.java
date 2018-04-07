f5c83d34592f189004baa27fd5e21fd811ff3260
Abd El Fattah <Abd El Fattah@Abdelfattah>
2018-04-07 15:41:59 +0200

    s

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/linkedList/cs25_cs26/JUnitOfPolynomial.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/linkedList/cs25_cs26/JUnitOfPolynomial.java
@@ -1,10 +1,10 @@
 package eg.edu.alexu.csd.datastructure.linkedList.cs25_cs26;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import org.junit.Test;
 
-import eg.edu.alexu.csd.datastructure.linkedList.ILinkedList;
 import eg.edu.alexu.csd.datastructure.linkedList.IPolynomialSolver;
 
 /**
@@ -18,21 +18,23 @@ public class JUnitOfPolynomial {
 	 * TO TEST POLY.
 	 */
 	@Test
-	public void cs55TestSizeContains() {
+	public void testSolveAdd() {
 		final IPolynomialSolver instance = (IPolynomialSolver) new Polynomial();
-		instance.setPolynomial('C', new int[][] {{3, 7 }, {45, 5 }, {176, 3 }, {128, 1 } });
-		instance.setPolynomial('B', new int[][] {{-120, 5 }, {-1, 3 }, {27, 2 }, {1, 1 }, {-1, 0 } });
-		instance.setPolynomial('A', new int[][] {{27, 2 }, {1, 1 }, {-1, 0 } });
+		instance.setPolynomial('C', new int[][] { { 3, 7 }, { 45, 5 }, { 176, 3 }, { 128, 1 } });
+		instance.setPolynomial('B', new int[][] { { -120, 5 }, { -1, 3 }, { 27, 2 }, { 1, 1 }, { -1, 0 } });
 		assertNull("Polynomial R is not set yet", instance.print('R'));
-		int[][] result1 = instance.add('C', 'B');
-		//float a = instance.evaluatePolynomial('A', 10);
-		assertEquals("27x^2+x-1", instance.print('A'));
-		int[][] expected = new int[][] {{3, 7 }, {-75, 5 }, {175, 3 }, {27, 2 }, {129, 1 }, {-1, 0 } };
-		System.out.println();
-		for (int i = 0; i < result1.length; i++) {
-			System.out.println(result1[i][1]);
-		}
-		String x = instance.print('R');
-		//assert.assertEquals(expected, result1);
+		assertEquals("3x^7+45x^5+176x^3+128x", instance.print('C'));
+	}
+
+	/**
+	 * TO TEST POLY2.
+	 */
+	@Test
+	public void testSolveMULTIBLY() {
+		final IPolynomialSolver instance = (IPolynomialSolver) new Polynomial();
+		instance.setPolynomial('A', new int[][] { { 0, 7 }, { 0, 5 }, { 0, 3 }, { 0, 1 } });
+		instance.setPolynomial('B', new int[][] { { 0, 5 }, { -1, 3 }, { 0, 2 }, { 1, 1 }, { -1, 0 } });
+		instance.multiply('A', 'B');
+		assertEquals("", instance.print('R'));
 	}
 }
