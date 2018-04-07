fe2eb69b606b3a33e6c0515a32353e4f4f0cf009
Abd El Fattah <Abd El Fattah@Abdelfattah>
2018-04-07 16:10:55 +0200

    s

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/linkedList/cs25_cs26/Polynomial.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/linkedList/cs25_cs26/Polynomial.java
@@ -835,7 +835,13 @@ public class Polynomial implements IPolynomialSolver {
 		}
 		int count = 0;
 		int maxExp = reArr[0][1];
-		int[][] multi1 = new int[r.size()][2];
+		int c = 0;
+		for (int ii = 0; ii < r.size(); ii++) {
+			if ((int) r.get(ii) != 0) {
+				c++;
+			}
+		}
+		int[][] multi1 = new int[c][2];
 		for (int j = 0; j < r.size(); j++) {
 			if ((int) r.get(j) != 0) {
 				multi1[count][0] = (int) r.get(j);
