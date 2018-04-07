700af0835db12d89af8aa0e71ddc81b8b056aca3
Abd El Fattah <Abd El Fattah@Abdelfattah>
2018-04-07 15:27:29 +0200

    s

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/linkedList/cs25_cs26/JUnitOfPolynomial.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/linkedList/cs25_cs26/JUnitOfPolynomial.java
@@ -1,10 +1,7 @@
 package eg.edu.alexu.csd.datastructure.linkedList.cs25_cs26;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 import eg.edu.alexu.csd.datastructure.linkedList.ILinkedList;
@@ -22,8 +19,7 @@ public class JUnitOfPolynomial {
 	 */
 	@Test
 	public void cs55TestSizeContains() {
-		IPolynomialSolver instance = (IPolynomialSolver) new Polynomial();
-		ILinkedList object = (ILinkedList) new SingleLinkedList();
+		final IPolynomialSolver instance = (IPolynomialSolver) new Polynomial();
 		instance.setPolynomial('C', new int[][] {{3, 7 }, {45, 5 }, {176, 3 }, {128, 1 } });
 		instance.setPolynomial('B', new int[][] {{-120, 5 }, {-1, 3 }, {27, 2 }, {1, 1 }, {-1, 0 } });
 		instance.setPolynomial('A', new int[][] {{27, 2 }, {1, 1 }, {-1, 0 } });
@@ -31,30 +27,12 @@ public class JUnitOfPolynomial {
 		int[][] result1 = instance.add('C', 'B');
 		//float a = instance.evaluatePolynomial('A', 10);
 		assertEquals("27x^2+x-1", instance.print('A'));
-		//int[][] expected = new int[][] {{3, 7 }, {-75, 5 }, {175, 3 }, {27, 2 }, {129, 1 }, {-1, 0 } };
+		int[][] expected = new int[][] {{3, 7 }, {-75, 5 }, {175, 3 }, {27, 2 }, {129, 1 }, {-1, 0 } };
 		System.out.println();
 		for (int i = 0; i < result1.length; i++) {
 			System.out.println(result1[i][1]);
 		}
 		String x = instance.print('R');
-		System.out.println();
-		System.out.println(x);
-	}
-
-	/**
-	 * TO TEST LINKED.
-	 */
-	@Test
-	public void cs56TestAddRemoveTwoLists2() {
-		ILinkedList c = (ILinkedList) new SingleLinkedList();
-		for (int i = 0; i < 3; i++) {
-			c.add(i, i);
-		}
-		ILinkedList d = (ILinkedList) new SingleLinkedList();
-		for (int i = 0; i < 3; i++) {
-			assertEquals(c.get(i), i);
-			//System.out.println(c.get(i));
-		}
-		 
+		assert.assertEquals(expected, result1);
 	}
 }
