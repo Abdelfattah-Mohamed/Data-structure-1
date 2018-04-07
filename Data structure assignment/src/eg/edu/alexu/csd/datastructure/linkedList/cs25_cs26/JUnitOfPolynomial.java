be2f915420014b2952323c6b38eb3e58b87d1d21
Abd El Fattah <Abd El Fattah@Abdelfattah>
2018-04-07 23:35:15 +0200

    pol

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/linkedList/cs25_cs26/JUnitOfPolynomial.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/linkedList/cs25_cs26/JUnitOfPolynomial.java
@@ -22,10 +22,10 @@ public class JUnitOfPolynomial {
 				(IPolynomialSolver) new Polynomial();
 		final int[][] c = {{3, 7 }, {45, 5 },
 				{176, 3 }, {128, 1 } };
-		final int[][] b ={{-120, 5 }, {-1, 3 },
+		final int[][] b = {{-120, 5 }, {-1, 3 },
 				{27, 2 }, {1, 1 }, {-1, 0 } };
-		instance.setPolynomial('C',c);
-		instance.setPolynomial('B',b);
+		instance.setPolynomial('C', c);
+		instance.setPolynomial('B', b);
 		assertNull("Polynomial R is not set yet",
 				instance.print('R'));
 		assertEquals("3x^7+45x^5+176x^3+128x",
@@ -39,12 +39,12 @@ public class JUnitOfPolynomial {
 	public void testSolveMULTIBLY() {
 		final IPolynomialSolver instance =
 				(IPolynomialSolver) new Polynomial();
-		instance.setPolynomial('A',
-				new int[][] {{0, 7 }, {0, 5 },
-			{0, 3 }, {0, 1 } });
-		instance.setPolynomial('B',
-				new int[][] {{0, 5 }, {-1, 3 },
-			{0, 2 }, {1, 1 }, {-1, 0 } });
+		final int[][] a = {{0, 7 }, {0, 5 },
+				{0, 3 }, {0, 1 } };
+		final int[][] b = {{0, 5 }, {-1, 3 },
+				{0, 2 }, {1, 1 }, {-1, 0 } };
+		instance.setPolynomial('A', a);
+		instance.setPolynomial('B', b);
 		instance.multiply('A', 'B');
 		assertEquals("", instance.print('R'));
 	}
