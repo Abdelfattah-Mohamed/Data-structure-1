b6e6de03ce510fcdbcc6d6d076648c707a7521f3
Abd El Fattah <Abd El Fattah@Abdelfattah>
2018-04-15 21:22:50 +0200

    s

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/stack/cs26/InfixPosfix.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/stack/cs26/InfixPosfix.java
@@ -75,9 +75,6 @@ public class InfixPosfix implements IExpressionEvaluator {
 		}
 		str1 += " ";
 		str1 += Character.toString((char) a.pop());
-		if (str1.length() == 0) {
-			str1 += " ";
-		}
 		return str1;
 	}
 
