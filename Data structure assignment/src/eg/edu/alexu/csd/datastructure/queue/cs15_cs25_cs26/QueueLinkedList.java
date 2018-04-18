a1a9c804f21d1220904ed23bc82e76defe0b03fc
Abd El Fattah <Abd El Fattah@Abdelfattah>
2018-04-18 14:24:27 +0200

    q

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/queue/cs15_cs25_cs26/QueueLinkedList.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/queue/cs15_cs25_cs26/QueueLinkedList.java
@@ -5,22 +5,18 @@ import eg.edu.alexu.csd.datastructure.queue.IQueue;
 import eg.edu.alexu.csd.datastructure.linkedList.*;
 import eg.edu.alexu.csd.datastructure.linkedList.cs25_cs26.Dlinkedlist;
 
-public class QueueLinkedList implements IQueue,ILinkedBased {
+public class QueueLinkedList implements IQueue, ILinkedBased {
 
 	final ILinkedList object = (ILinkedList) new Dlinkedlist();
 
-	public QueueLinkedList() {
-		// TODO Auto-generated constructor stub
-	}
-
 	@Override
-	public void enqueue(Object item) {
+	public final void enqueue(final Object item) {
 		// TODO Auto-generated method stub
 		object.add(item);
 	}
 
 	@Override
-	public Object dequeue() {
+	public final Object dequeue() {
 		// TODO Auto-generated method stub
 		Object x = object.get(0);
 		object.remove(0);
@@ -28,13 +24,13 @@ public class QueueLinkedList implements IQueue,ILinkedBased {
 	}
 
 	@Override
-	public boolean isEmpty() {
+	public final boolean isEmpty() {
 		// TODO Auto-generated method stub
 		return object.isEmpty();
 	}
 
 	@Override
-	public int size() {
+	public final int size() {
 		// TODO Auto-generated method stub
 		return object.size();
 	}
