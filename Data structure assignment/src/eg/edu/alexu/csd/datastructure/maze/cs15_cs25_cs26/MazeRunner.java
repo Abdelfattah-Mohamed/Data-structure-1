0ae0130cd765ef7f49746a5db4e4e9df76e36e2d
Abd El Fattah <Abd El Fattah@Abdelfattah>
2018-04-21 00:47:10 +0200

    test 5

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/maze/cs15_cs25_cs26/MazeRunner.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/maze/cs15_cs25_cs26/MazeRunner.java
@@ -186,7 +186,7 @@ public class MazeRunner implements IMazeSolver {
 			}
 			return ret;
 		} catch (Exception e) {
-			throw new RuntimeException();
+			throw new RuntimeException("null");
 		}
 	}
 
