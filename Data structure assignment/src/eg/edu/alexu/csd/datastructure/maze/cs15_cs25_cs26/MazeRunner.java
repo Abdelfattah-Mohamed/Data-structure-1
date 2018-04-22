053305c84975050cfeac6054fdbdd3114eed83f6
Abd El Fattah <Abd El Fattah@Abdelfattah>
2018-04-22 04:48:52 +0200

    s

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/maze/cs15_cs25_cs26/MazeRunner.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/maze/cs15_cs25_cs26/MazeRunner.java
@@ -11,28 +11,62 @@ import eg.edu.alexu.csd.datastructure.queue.cs15_cs25_cs26.QueueLinkedList;
 import eg.edu.alexu.csd.datastructure.stack.IStack;
 import eg.edu.alexu.csd.datastructure.stack.cs26.Stack;
 
+/**
+ * @author Abd Elfattah
+ * @author super
+ * @author Abd Elrahman
+ */
 class Node {
-	Point currentNode;
-	Node prevNode;
+	/**
+	 * the current cell.
+	 */
+	private Point currentNode;
+	/**
+	 * the previous cell.
+	 */
+	private Node prevNode;
 
-	Node(Point cur, Node preNode) {
+	/**
+	 * constructor for the Node.
+	 *
+	 * @param cur
+	 *            the current cell.
+	 * @param preNode
+	 *            the previous Node.
+	 */
+	Node(final Point cur, final Node preNode) {
 		currentNode = cur;
 		prevNode = preNode;
 	}
 
+	/**
+	 * getting the current cell.
+	 *
+	 * @return the current cell
+	 */
 	Point getCurrent() {
 		return currentNode;
 	}
 
+	/**
+	 * getting the previous cell.
+	 *
+	 * @return the previous cell
+	 */
 	Node getPreNode() {
 		return prevNode;
 	}
 }
 
+/**
+ * @author Abd Elrahamn
+ * @author super
+ * @author Abd Elfattah
+ */
 public class MazeRunner implements IMazeSolver {
 
 	@Override
-	public int[][] solveBFS(File maze) {
+	public final int[][] solveBFS(final File maze) {
 		try {
 			char[][] mazeCh = readCharArray(maze);
 			int rows = mazeCh.length;
@@ -40,59 +74,76 @@ public class MazeRunner implements IMazeSolver {
 			Point start = findStart(mazeCh);
 			Point end = findEnd(mazeCh);
 			boolean[][] visited = new boolean[rows][cols];
-			for (int i = 0; i < rows; i++) {
-				for (int j = 0; j < cols; j++) {
-					visited[i][j] = false;
-				}
-			}
-			IQueue Q = new QueueLinkedList();
-			IStack S = new Stack();
+			IQueue q = new QueueLinkedList();
+			IStack s = new Stack();
 			Node begin = new Node(start, null);
-			Q.enqueue(begin);
-			S.push(begin);
+			q.enqueue(begin);
+			s.push(begin);
 			visited[start.x][start.y] = true;
-			while (!Q.isEmpty()) {
-				Node current = (Node) Q.dequeue();
+			while (!q.isEmpty()) {
+				Node current = (Node) q.dequeue();
 				Point test = current.getCurrent();
 				if (test.x == end.x && test.y == end.y) {
 					visited[end.x][end.y] = true;
-					S.push(current);
-					return drawPath(S, end.x, end.y);
+					s.push(current);
+					return drawPath(s);
 				} else {
-					if (validPath(rows, cols, test.x + 1, test.y)) {
-						if (!visited[test.x + 1][test.y] && mazeCh[test.x + 1][test.y] != '#') {
-							visited[test.x + 1][test.y] = true;
-							Point p = new Point(test.x + 1, test.y);
-							Node cell = new Node(p, current);
-							Q.enqueue(cell);
-							S.push(cell);
+					if (validPath(rows,
+							cols,
+							test.x + 1, test.y)) {
+						if (!visited[test.x + 1][test.y]
+					&& mazeCh[test.x + 1][test.y] != '#') {
+							visited[test.x + 1]
+								[test.y] = true;
+							Point p
+							= new Point(
+							test.x + 1, test.y);
+							Node cell
+							= new Node(p, current);
+							q.enqueue(cell);
+							s.push(cell);
 						}
 					}
-					if (validPath(rows, cols, test.x, test.y + 1)) {
-						if (!visited[test.x][test.y + 1] && mazeCh[test.x][test.y + 1] != '#') {
-							visited[test.x][test.y + 1] = true;
-							Point p = new Point(test.x, test.y + 1);
-							Node cell = new Node(p, current);
-							Q.enqueue(cell);
-							S.push(cell);
+					if (validPath(
+					rows, cols, test.x, test.y + 1)) {
+						if (!visited[test.x][test.y + 1]
+					&& mazeCh[test.x][test.y + 1] != '#') {
+							visited[test.x]
+							[test.y + 1] = true;
+							Point p = new Point(
+							test.x, test.y + 1);
+							Node cell
+							= new Node(p, current);
+							q.enqueue(cell);
+							s.push(cell);
 						}
 					}
-					if (validPath(rows, cols, test.x - 1, test.y)) {
-						if (!visited[test.x - 1][test.y] && mazeCh[test.x - 1][test.y] != '#') {
-							visited[test.x - 1][test.y] = true;
-							Point p = new Point(test.x - 1, test.y);
-							Node cell = new Node(p, current);
-							Q.enqueue(cell);
-							S.push(cell);
+					if (validPath(
+					rows, cols, test.x - 1, test.y)) {
+						if (!visited[test.x - 1][test.y]
+					&& mazeCh[test.x - 1][test.y] != '#') {
+							visited[test.x - 1]
+							[test.y] = true;
+							Point p = new Point(
+							test.x - 1, test.y);
+							Node cell
+							= new Node(p, current);
+							q.enqueue(cell);
+							s.push(cell);
 						}
 					}
-					if (validPath(rows, cols, test.x, test.y - 1)) {
-						if (!visited[test.x][test.y - 1] && mazeCh[test.x][test.y - 1] != '#') {
-							visited[test.x][test.y - 1] = true;
-							Point p = new Point(test.x, test.y - 1);
-							Node cell = new Node(p, current);
-							Q.enqueue(cell);
-							S.push(cell);
+					if (validPath(
+					rows, cols, test.x, test.y - 1)) {
+						if (!visited[test.x][test.y - 1]
+				&& mazeCh[test.x][test.y - 1] != '#') {
+							visited[test.x]
+							[test.y - 1] = true;
+							Point p = new Point(
+							test.x, test.y - 1);
+							Node cell
+							= new Node(p, current);
+							q.enqueue(cell);
+							s.push(cell);
 						}
 					}
 				}
@@ -104,14 +155,16 @@ public class MazeRunner implements IMazeSolver {
 	}
 
 	@Override
-	public int[][] solveDFS(final File maze) {
+	public final int[][] solveDFS(final File maze) {
 		// TODO Auto-generated method stub
 		try {
 			char[][] mazeCh = readCharArray(maze);
 			Point start, end;
 			int si = 0;
 			int sj = 0;
-			boolean[][] tf = new boolean[mazeCh.length][mazeCh[0].length];
+			boolean[][] tf = new boolean
+					[mazeCh.length]
+					[mazeCh[0].length];
 			start = findStart(mazeCh);
 			end = findEnd(mazeCh);
 			si = start.x;
@@ -125,8 +178,11 @@ public class MazeRunner implements IMazeSolver {
 				si = (int) inI.peek();
 				sj = (int) inJ.peek();
 				if (si != end.x || sj != end.y) {
-					if (validPath(mazeCh.length, mazeCh[0].length, si + 1, sj)
-							&& (!tf[si + 1][sj] && mazeCh[si + 1][sj] != '#')) {
+					if (validPath(mazeCh.length,
+							mazeCh[0].length,
+							si + 1, sj)
+							&& (!tf[si + 1][sj]
+					&& mazeCh[si + 1][sj] != '#')) {
 						si++;
 						tf[si][sj] = true;
 						inI.push(si);
@@ -175,26 +231,33 @@ public class MazeRunner implements IMazeSolver {
 		}
 	}
 
-	private char[][] readCharArray(File maze) {
+	/**
+	 * reading from file.
+	 *
+	 * @param maze
+	 *            file destination
+	 * @return char 2D array
+	 */
+	private char[][] readCharArray(final File maze) {
 		try {
 			FileReader mazeFile = new FileReader(maze);
 			@SuppressWarnings("resource")
 			BufferedReader br = new BufferedReader(mazeFile);
 			String size = br.readLine();
 			char[] sizeCh = size.toCharArray();
-			StringBuilder M = new StringBuilder();
-			StringBuilder N = new StringBuilder();
+			StringBuilder m = new StringBuilder();
+			StringBuilder n = new StringBuilder();
 			boolean flag = true;
 			for (int i = 0; i < sizeCh.length; i++) {
 				if (flag) {
 					while (sizeCh[i] != ' ') {
-						M.append(sizeCh[i]);
+						m.append(sizeCh[i]);
 						i++;
 					}
 					flag = false;
 				} else {
 					while (sizeCh[i] != ' ') {
-						N.append(sizeCh[i]);
+						n.append(sizeCh[i]);
 						i++;
 						if (i == sizeCh.length) {
 							break;
@@ -202,27 +265,40 @@ public class MazeRunner implements IMazeSolver {
 					}
 				}
 			}
-			int m = Integer.parseInt(M.toString());
-			int n = Integer.parseInt(N.toString());
-			char[][] mazeCh = new char[m][n];
-			String[] mazeStr = new String[m];
-			for (int i = 0; i < m; i++) {
+			int r = Integer.parseInt(m.toString());
+			int c = Integer.parseInt(n.toString());
+			char[][] mazeCh = new char[r][c];
+			String[] mazeStr = new String[r];
+			for (int i = 0; i < r; i++) {
 				mazeStr[i] = br.readLine();
 			}
 			if (br.readLine() != null) {
 				throw new RuntimeException();
 			}
-			mazeCh = toCharArray(mazeStr, m, n);
+			mazeCh = toCharArray(mazeStr, r, c);
 			return mazeCh;
 		} catch (Exception e) {
 			throw new RuntimeException();
 		}
 	}
 
-	private char[][] toCharArray(String[] mazeStr, int m, int n) {
+	/**
+	 * converting array of string to 2-D char array.
+	 *
+	 * @param mazeStr
+	 *            array of string
+	 * @param m
+	 *            number of rows
+	 * @param n
+	 *            number of columns
+	 * @return 2-D char array "maze"
+	 */
+	private char[][] toCharArray(
+		final String[] mazeStr, final int m, final int n) {
 		char[][] mazeCh = new char[m][n];
 		for (int i = 0; i < mazeStr.length; i++) {
-			char[] temp = mazeStr[i].replaceAll("\\s", "").toCharArray();
+			char[] temp
+			= mazeStr[i].replaceAll("\\s", "").toCharArray();
 			if (temp.length != n) {
 				throw new RuntimeException();
 			}
@@ -233,12 +309,20 @@ public class MazeRunner implements IMazeSolver {
 		return mazeCh;
 	}
 
-	private Point findStart(char[][] mazeCh) {
+	/**
+	 * finding starting point.
+	 *
+	 * @param mazeCh
+	 *            array of char containing the maze
+	 * @return the start point if the start doesn't return error
+	 */
+	private Point findStart(final char[][] mazeCh) {
 		Point start = new Point();
 		boolean flag = false;
 		for (int i = 0; i < mazeCh.length; i++) {
 			for (int j = 0; j < mazeCh[0].length; j++) {
-				if (mazeCh[i][j] == 'S' || mazeCh[i][j] == 's') {
+				if (mazeCh[i][j] == 'S'
+				|| mazeCh[i][j] == 's') {
 					start.x = i;
 					start.y = j;
 					flag = true;
@@ -256,12 +340,20 @@ public class MazeRunner implements IMazeSolver {
 		}
 	}
 
-	private Point findEnd(char[][] mazeCh) {
+	/**
+	 * finding ending point.
+	 *
+	 * @param mazeCh
+	 *            array of char containing the maze
+	 * @return the end point if the start doesn't return error
+	 */
+	private Point findEnd(final char[][] mazeCh) {
 		Point end = new Point();
 		boolean flag = false;
 		for (int i = 0; i < mazeCh.length; i++) {
 			for (int j = 0; j < mazeCh[0].length; j++) {
-				if (mazeCh[i][j] == 'E' || mazeCh[i][j] == 'e') {
+				if (mazeCh[i][j] == 'E'
+				|| mazeCh[i][j] == 'e') {
 					end.x = i;
 					end.y = j;
 					flag = true;
@@ -279,13 +371,34 @@ public class MazeRunner implements IMazeSolver {
 		}
 	}
 
-	private boolean validPath(int rows, int cols, int row, int col) {
+	/**
+	 * checking boundaries.
+	 *
+	 * @param rows
+	 *            number of rows
+	 * @param cols
+	 *            number of columns
+	 * @param row
+	 *            the current row
+	 * @param col
+	 *            the current column
+	 * @return true if the cell is in the boundaries else false
+	 */
+	private boolean validPath(
+	final int rows, final int cols, final int row, final int col) {
 		return (row >= 0 && row < rows && col >= 0 && col < cols);
 	}
 
-	private int[][] drawPath(IStack S, int x, int y) {
+	/**
+	 * drawing the path we found in the maze.
+	 *
+	 * @param s
+	 *            stack containing the points
+	 * @return 2-D integer array containing the path
+	 */
+	private int[][] drawPath(final IStack s) {
 		IStack temp = new Stack();
-		Node test = (Node) S.peek();
+		Node test = (Node) s.peek();
 		while (test != null) {
 			temp.push(test.getCurrent());
 			test = test.getPreNode();
@@ -300,4 +413,5 @@ public class MazeRunner implements IMazeSolver {
 		}
 		return path;
 	}
+
 }
