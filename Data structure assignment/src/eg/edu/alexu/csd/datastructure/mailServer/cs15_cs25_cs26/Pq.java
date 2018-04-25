d4f56eebf7979a3dda2818bb1ec2329e59ecb0f6
Abd El Fattah <Abd El Fattah@Abdelfattah>
2018-04-25 22:42:26 +0200

    style 4

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/mailServer/cs15_cs25_cs26/Pq.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/mailServer/cs15_cs25_cs26/Pq.java
@@ -1,6 +1,7 @@
 package eg.edu.alexu.csd.datastructure.mailServer.cs15_cs25_cs26;
+
 /**
- * 
+ *
  * @author Abd El Fattah
  * @author Abd El Rahman
  * @author Gamal Eldien
@@ -16,6 +17,13 @@ public class Pq {
 	 */
 	int key;
 
+	/**
+	 * 
+	 * @param value
+	 *            to put item in element
+	 * @param pt
+	 *            to put key in key
+	 */
 	public Pq(final Object value, final int pt) {
 		// TODO Auto-generated constructor stub
 		element = value;
