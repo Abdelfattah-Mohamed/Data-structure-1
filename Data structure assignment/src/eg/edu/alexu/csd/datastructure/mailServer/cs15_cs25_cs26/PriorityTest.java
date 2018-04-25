05832f7744a69328eece88e6ba7a7a1add700201
Abd El Fattah <Abd El Fattah@Abdelfattah>
2018-04-25 20:37:20 +0200

    TEST 1

--- Data structure assignment/src/eg/edu/alexu/csd/datastructure/mailServer/cs15_cs25_cs26/PriorityTest.java
+++ Data structure assignment/src/eg/edu/alexu/csd/datastructure/mailServer/cs15_cs25_cs26/PriorityTest.java
@@ -1,19 +1,21 @@
-/*
 package eg.edu.alexu.csd.datastructure.mailServer.cs15_cs25_cs26;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import eg.edu.alexu.csd.datastructure.mailServer.IPriorityQueue;
 
-class PriorityTest {
-
+/**
+ * @author Abd El Fattah
+ */
+public class PriorityTest {
+	/**
+	 * @Test
+	 */
 	@Test
-	void test() {
+	public void test() {
 		IPriorityQueue test = new PriorityQueue();
 		for (int i = 0; i < 10; i++) {
 			test.insert(i, i);
 		}
 	}
-
 }
-*/
